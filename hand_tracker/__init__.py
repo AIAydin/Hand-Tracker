3fab7390d4ea2dca7f32d6f36aee40becea3ca8d
AIAydin <aydinfs33@gmail.com>
2025-11-08 15:56:06 -0500

    Expose all modules in package __all__

--- hand_tracker/__init__.py
+++ hand_tracker/__init__.py
@@ -2,6 +2,16 @@
 Hand Tracker package
 """
 
-__all__ = ["app", "camera", "hands", "overlay"]
+__all__ = [
+    "app",
+    "camera",
+    "hands",
+    "overlay",
+    "gestures",
+    "virtual_mouse",
+    "slides",
+    "games",
+    "launcher",
+]
 __version__ = "0.1.0"
 
